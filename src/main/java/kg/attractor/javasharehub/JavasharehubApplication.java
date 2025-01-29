37878dab7128019640f914540032bd083370a179
Sedep <yagaminezukochan@gmail.com>
2025-01-29 12:30:28 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/javasharehub/JavasharehubApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.javasharehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasharehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavasharehubApplication.class, args);
+    }
+
+}
